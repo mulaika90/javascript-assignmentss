09579c462fe5b1d125df0d81eeee5f29d3600829
Your Name <you@example.com>
2025-12-29 14:09:24 +0500

    updated

---
+++ chapter9 to 11/app.js
@@ -0,0 +1,133 @@
+var cityname = prompt("Enter our city name");
+if("cityname === karachi"){
+    console.log("welcom to city of light");
+}
+var gender = prompt("enter your gender");
+if("gender === male"){
+    console.log("Good morning Sir");
+    
+}
+if("gender === female"){
+    console.log("Good morning mam");
+    
+}
+var traffic = prompt("color of traffic signal");
+if("red,yellow,green"){
+    console.log("red= must stop");
+    console.log("yellow= ready to move");
+    console.log("green= move now");
+}
+var fuel = prompt("remaining fuel in car");
+if("fuel<0.25"){
+    console.log("please refill the fuel in your car");
+}
+var a = 4;
+if(++a === 5){
+    alert("given codition for variable is true");
+}
+var b = 82;
+if(b++ === 13){
+    alert("given condition for variable b is true")
+}
+var c = 12;
+if(c++ === 13){
+   alert("condition 1 is true")
+}
+if(c === 13){
+    alert("condition 2 is true")
+}
+if(++c<14){
+    alert("condition 3 is true")
+}
+if(c === 14){
+    alert("condition 4 is true")
+}
+var materialcost = 2000;
+var laborcost = 2000;
+var totalcost = materialcost+laborcost;
+if(totalcost === laborcost+materialcost){
+    alert("the cost equals");
+}
+if(true){
+    alert("true");
+
+}
+if(false){
+    alert("false");
+}
+if("car<cat"){
+    alert("car is smaller than cat");
+}
+var grade = prompt("marks obtained");
+if("percentage>==80"){
+    console.log("grade: A-one"+" remarks: excellent");
+    
+}
+if("percentage>==70"){
+    console.log("Geade: A"+" remarks: Good");
+    
+}
+if("percentage>==60"){
+    console.log("grade: B"+" remarks: you need to improve");
+}
+if("percentage<60"){
+    console.log("grade: fail"+" remarks: sorry");
+    
+}
+document.write("<h1>Marks Sheet</h1>"+" <p>Total Marks: 300</p>"+" <p>Marks obtained: 219</p>"+" <p>parcentage: 73%</p>"+" <p>Grade: B</p>"+"<p>Remarks: you need to improve</p>")
+var secret = prompt("guess the secret number");
+if("guess === 2"){
+    console.log("bingo! correct answer");
+}
+if("guess === +1"){
+    console.log("close enough to the correct answer");
+    
+}
+var num3 = prompt("the number3 is divisible by 3");
+if("yes"){
+    console.log("the number is divisible by 3");
+    
+}
+var num4 = prompt("the number2 is even or odd");
+if("even"){
+    console.log("correct");
+    
+}
+if("odd"){
+    console.log("un correct");
+    
+}
+var temperature = prompt("write temperature");
+if("t>40"){
+    console.log("it is too hot outside");
+    
+}
+if("t>30"){
+    console.log("the weather today is normal");
+    
+}
+if("t>20"){
+    console.log("today weather is cool");
+    
+}
+if("t>10"){
+    console.log("OMG today weather is so cool");
+    
+}
+var num1 = prompt("write first number");
+var num2 = prompt("write second number");
+var operation = "+";
+if("num1+num2"){
+    console.log("3");
+    
+}
+
+
+
+
+
+
+
+
+
+
