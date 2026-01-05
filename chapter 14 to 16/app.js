940d095d44729decb6dafec585a04bb3a1ec3344
Your Name <you@example.com>
2026-01-05 13:39:46 +0500

    javascript

---
+++ chapter 14 to 16/app.js
@@ -0,0 +1,143 @@
+var array = ["purple ","blue"," red","yellow ","green ","silver "]
+console.log(array);
+var number = [1,2,3,4,5,6,7]
+console.log(number);
+var boo = [true,false]
+console.log(boo);
+var mixed = ["answer "+"of "+"2+true "+"is "+"3"]
+console.log(mixed);
+var education = ["SCC ","HCS ","BCS ","BS ","B.COM ","MS ","M.PHIL ","PHD"]
+console.log(education);
+document.write("<p>SCC</p> "+"<p>HCS</p> "+"<p>BCS</p> "+"<p>BS</p> "+"<p>B.COM</p> "+"<p>MS</p> "+"<p>M.PHIL</p> "+"<p>PHD</p>")
+var students = ["sara","maria","ayesha"]
+console.log(students);
+var score = [320,230,480]
+console.log(score);
+var total = "500"
+console.log(320/500*100);
+console.log(230/500*100);
+console.log(480/500*100);
+document.write("<p>score of sara is 320.percentage: 64%</p> "+"<p>score of maria is 230.percentage: 46%</p> "+"<p>"+"<p>score of ayesha is 480.percentage: 96%</p> ")
+var number = [320,230,480,120]
+number[0] = "120";
+number[1] = "230";
+number[2] = "320";
+number[3] = "480";
+console.log(number);
+document.write("<p>Scores of student: 320,230,480,120</p> "+"<p>ordered score of student: 120,230,320,480</p>")
+var cities = ["Lahore","Karachi","Peshawar","Islamabad"]
+var selected = ["lahore","karachi","islamabad"]
+console.log(selected);
+document.write("<h3>cities list</h3>"+"<p>Lahore,Karachi,Peshawar,Islamabad</p>"+"<h3>Selected cities list</h3>"+"<p>Lahore,Karachi,Islamabad")
+var arr = ["this is my cat"]
+console.log(arr);
+document.write("<h4>Array</h4>"+"<p>this,is,my,cat</p>"+"<h4>String</h4>"+"<p>This is my cat</p>")
+var stored = ["Keyboard","Mouse","Printer","Monitor"] 
+stored[0] = "monitor";
+stored[1] = "printer";
+stored[2] = "mouse";
+stored[3] = "keyboard";
+console.log(stored);
+document.write("<h3>Devices</h3>"+"<p>Keyboard,Mouse,Printer,Monitor"+"<h3>out</h3>"+"<p>Keyboard</p>"+"<p>Mouse</p>"+"<p>Printer</p>"+"<p>monitor</p>")
+document.write("<h3>Devices</h3>"+"<p>Keyboard,Mouse,Printer,Monitor"+"<h3>out</h3>"+"<p>monitor</p>"+"<p>printer</p>"+"<p>mouse</p>"+"<p>keyboard</p>")
+var mobile = ["apple","samsung","motorola","nokia","sony","haier"]
+document.write("<p>Apple</p>"+"<p>Samsung</p>"+"<p>Motorola</p>"+"<p>Nokia</p>"+"<p>Sony</p>"+"<p>Haier</p>")
+var add = prompt("what colour you want to add to the beginning")
+var color = ["red","blue","green","purple","pink"]
+console.log(color);
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.push("pink")
+color.push("purple")
+color.push("green")
+color.push("blue")
+color.push("red")
+
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.push("silver")
+color.push("golden")
+color.push("pink")
+color.push("purple")
+color.push("green")
+color.push("blue")
+color.push("red")
+document.write("<p>silver ","golden ","pink ","purple ","green ","blue ","red </p>")
+
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.pop()
+color.push("golden")
+color.push("pink")
+color.push("purple")
+color.push("green")
+color.push("blue")
+color.push("red")
+
+color.pop()
+
+if(add == "pink"){
+    document.write("pink ","purple ","green ","blue ","red")
+     
+}
+var end = prompt("what colour you want to add to the end")
+var colour = ["red","blue","green","purple","pink"]
+colour.pop()
+colour.pop()
+colour.pop()
+colour.pop()
+colour.pop()
+colour.push("red")
+colour.push("blue")
+colour.push("green")
+colour.push("purple")
+colour.push("pink")
+
+if(end == "pink"){
+    document.write("<p>red ","blue ","green ","purple ","pink</p> ")
+}
+console.log(colour);
+
+var index = prompt("at which index you want to add to add a colour and color name")
+var num = ["mehroon","black","blue"]
+console.log(num);
+   num.pop()
+   num.pop()
+   num.pop()
+   num.pop()
+      num.pop()
+num.push("black")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
