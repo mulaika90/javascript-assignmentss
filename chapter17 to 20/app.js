4f309398b80322e48ae3864aa1f18e27724c05f1
Your Name <you@example.com>
2026-01-09 13:02:22 +0500

    javascript

---
+++ chapter17 to 20/app.js
@@ -0,0 +1,13 @@
+var empty = [[]]
+console.log(empty);
+var matrix = [[0 ,1 ,2 ,3],[1 ,0 ,1 ,2],[2 ,1 ,0 ,1]]
+console.log(matrix);
+document.write(`<p>${matrix[0]}</p>`)
+document.write(`<p>${matrix[1]}</p>`)
+document.write(`<p>${matrix[2]}</p>`)
+
+for( var i = 1; i <= 10; i++){
+    console.log(i);
+    document.write(`<p>${i}</p>`)
+    
+}
