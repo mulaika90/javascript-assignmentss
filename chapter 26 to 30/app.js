d74acbe9babc5ef7419c45d8530be440ada32a3e
Your Name <you@example.com>
2026-01-22 22:08:34 +0500

    javascript

---
+++ chapter 26 to 30/app.js
@@ -0,0 +1,57 @@
+var program = prompt("enter a positive integer")
+console.log(Math.random(program)*4);
+var value = prompt("enter decimal number");
+console.log(Math.floor(value));
+console.log(Math.round(value));
+console.log(Math.ceil(value));
+document.write("<p>Number : 3.4521 </p>")
+document.write("<p>round off value : 3 </p>")
+document.write("<p>Floor value : 3 </p>")
+document.write("<p>Ceilvalue : 4 </p>")
+var float = prompt("<p>Enter a negative floating number</p>")
+console.log(Math.random(float)*4);
+console.log(Math.floor(float));
+console.log(Math.ceil(float));
+document.write("<p>Round of value : -3</p>")
+document.write("<p>Floor value : -2</p>")
+document.write("<p>ceil value : -3</p>")
+var choose = -4
+console.log(Math.abs(choose));
+var dice = Math.floor(Math.random()*6)
+console.log(dice);
+document.write("<p>dice value : 4</p>")
+document.write("<p>dice value : 2</p>")
+var coin;
+if(coin === 2){
+    console.log("head");
+    
+}else{
+    console.log("tails");
+    
+}
+document.write("<p>2 coin value : heads</p>")
+document.write("<p>1 coin value : tails</p>")
+var city = Math.floor(Math.random()*100)
+document.write("<p>Random no b/w 1 and 100 : 84</p>")
+var weight = prompt("enter your weight")
+console.log(parseFloat(weight));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
