984c80e5e5d7eba3f495e569f88d7b290fe9e550
Your Name <you@example.com>
2026-01-25 13:58:29 +0500

    javascript

---
+++ chapter 31 to 34/app.js
@@ -0,0 +1,30 @@
+var date = new Date()
+console.log(date);
+document.write(date+"<br>")
+var miss = ["January","february","march","april","may","june","july","august","september","october","november","december"]
+var date = new Date()
+var now = date.getMonth()
+console.log(miss[now]);
+document.write(miss[0]);
+var state = new Date()
+statestr = state.toString()
+var day = statestr.slice(0,4)
+console.log(day);
+document.write("<p>today is sun</p>")
+var promise = ["staturday","sunday"]
+var ask = prompt("enter day")
+if(promise === promise){
+    console.log("it is fun day");
+    
+}else if(promise === promise){
+    console.log("it is fun day");
+    
+}else{
+    console.log("bad day");
+    
+}
+document.write("<p>It is fun day</p>")
+
+
+
+
