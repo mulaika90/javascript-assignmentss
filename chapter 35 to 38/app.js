87e03caf5a6b680b4b85cfc4505b8337546cba03
Your Name <you@example.com>
2026-02-04 13:21:01 +0500

    javascript

---
+++ chapter 35 to 38/app.js
@@ -0,0 +1,25 @@
+function greet(){
+    var date = new Date()
+    console.log(date);
+    
+}
+greet()
+function greetUser() {
+  var firstName = prompt("Enter your first name:");
+  var lastName = prompt("Enter your last name:");
+  console.log(firstName,lastName);
+  
+}
+greetUser()
+function add(a,b){
+ console.log(a+b);
+ 
+}
+add(2,4)
+function sum(a,b){
+    console.log(a*b);
+    
+}
+sum(2,4)
+
+
