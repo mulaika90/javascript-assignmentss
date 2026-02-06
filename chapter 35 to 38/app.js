3993773cff22e4d792b96fc2a0e9e5cae5b96e17
Your Name <you@example.com>
2026-02-06 13:37:56 +0500

    javascript

--- chapter 35 to 38/app.js
+++ chapter 35 to 38/app.js
@@ -11,15 +11,45 @@ function greetUser() {
   
 }
 greetUser()
-function add(a,b){
- console.log(a+b);
- 
-}
-add(2,4)
-function sum(a,b){
-    console.log(a*b);
+function calculation(num1,num2,operator){
+    var operation;
+    if(operator == "+"){
+        operation = num1 + num2;
+    }else if(operator == "-") {
+        operation = num1 - num2;
+    }else if(operator == "*") {
+        operation = num1 * num2;
+    }else if(operator == "/") {
+        operation = num1 / num2;
+    } 
+    else{
+        operation = "invalid operator"
+    }
     
+
+
+
 }
-sum(2,4)
+var userNum1 = Number(prompt("enter a number"))
+var userNum2 = Number(prompt("enter a  another number"))
+var userOperator = (prompt("enter a operator(+,-,*,/)"))
+
+console.log(calculation(userNum1,userNum2,userOperator));
+
+function calculatesquare(x){
+    return x**2
+}
+console.log(calculatesquare(5));
+
+function factorial(n){
+    var result = 1
+    for(var i = 1; i<=n; i++){
+        result == result*1
+        return result
+    }
+}
+console.log(factorial(5));
+
+
 
 
