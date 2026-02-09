395fb595937d0f132e9bf33909437df36ca26027
Your Name <you@example.com>
2026-02-09 13:36:08 +0500

    javascript

---
+++ calculator/app.js
@@ -0,0 +1,25 @@
+var display = document.getElementById("display")
+var firstnumber;
+var secondnumber;
+var operator;
+var result;
+function calculate(){
+    if(operator == "+"){
+        result = firstnumber+secondnumber
+    }else if(operator == "-"){
+        result = firstnumber-secondnumber
+    }else if(operator == "/"){
+        result = firstnumber/secondnumber
+    }else if(operator == "*"){
+        result = firstnumber*secondnumber
+    }else    if(operator == "."){
+        result = firstnumber.secondnumber
+    } else   if(operator == "%"){
+        result = firstnumber%secondnumber
+    }
+
+
+
+
+
+}
